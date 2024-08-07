754a24fe2dd5ea5e9f8d366b5ccd30c4cf17cdc0
Akash P <akashpolineni@gmail.com>
2024-08-07 16:15:56 +0530

    phase 1 is updated in the model_training with confirmed roc,acu and f1 scores

--- src/model_training.py
+++ src/model_training.py
@@ -4,12 +4,12 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, roc_auc_score
 
 def load_data(path):
-    data=pd.read_csv(path)
+    data=pd.read_csv(r"D:\\PulmonaryInfarction\\multimodalpulmonaryembolismdataset\\processed_multimodalpulmonaryembolismdataset.csv")
     return data
 
 def split_data(data):
-    X = data.drop('target_column', axis=1)  # Replace 'target_column' with your target column name
-    y = data['target_column']
+    X = data.drop('DISEASES OF PULMONARY CIRCULATION:presence', axis=1)  # Replace 'target_column' with your target column name
+    y = data['DISEASES OF PULMONARY CIRCULATION:presence']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
@@ -26,12 +26,12 @@ def evaluate_model(model, X_test, y_test):
     return accuracy, cm, roc_auc
 
 if __name__=="__main__":
-    processed_data_path="D:\\PulmonaryInfarction\\multimodalpulmonaryembolismdataset\\processed_multimodalpulmonaryembolismdataset.csv"
+    processed_data_path=r"D:\\PulmonaryInfarction\\multimodalpulmonaryembolismdataset\\processed_multimodalpulmonaryembolismdataset.csv"
     data=load_data(processed_data_path)
     X_train,X_test,y_train,y_test = split_data(data)
 
     model=train_model(X_train, y_train)
-    acuracy,cm,roc_auc= evaluate_model(model,X_test,y_test)
+    accuracy,cm,roc_auc= evaluate_model(model,X_test,y_test)
 
     print(f"Accuracy: {accuracy}")
     print(f"Confusion Matrix: \n{cm}")
