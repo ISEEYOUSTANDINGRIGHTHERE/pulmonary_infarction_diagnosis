a7f7590b97beffb82664127005a64b357630a1bd
Akash P <akashpolineni@gmail.com>
2025-04-14 14:37:03 +0530

    rewrite

--- phase_2/src/xai/grad_cam.py
+++
@@ -1,9 +0,0 @@
-import torch 
-import numpy as np
-import cv2
-from grad_cam import GradCAM
-
-def apply_gradcam(model,image_tensor, target layer):
-    gradcam= GradCAM(model,target layer)
-    heatmap=gradcam(image_tensor)
-    return cv2.applyColorMap(np.uint8(255*heatmap),cv2.COLORMAP_JET)
--- src/Untitled-2.py
+++
@@ -1,5 +0,0 @@
-def load_data(path):
-    data = pd.read_csv("D:\\PulmonaryInfarction\\multimodalpulmonaryembolismdatase\\ICD.csv")
-    print(data.dtypes)  # Print the data types of each column
-    return data
-    
